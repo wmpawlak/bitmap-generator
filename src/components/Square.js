5b1a0cdee07831b35cb12fceeb080627a4163690
Wojtek Pawlak <wojciech.pawlak92@gmail.com>
2019-06-29 19:05:49 +0200

    added components: buttons, palette and board

---
+++ src/components/Square.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
+    render() {
+        return (
+            <button
+                className="square"
+                onClick={() => this.setState({ value: 'X' })}
+            >
+                {this.state.value}
+            </button>
+        );
+    }
+}
+
+export default Square;
