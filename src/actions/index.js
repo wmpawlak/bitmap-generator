f1a84e29862fc55107034a04e1f81deb68a79c6f
Wojtek Pawlak <wojciech.pawlak92@gmail.com>
2019-06-29 15:56:55 +0200

    structure of a project established

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+export const colorAssigned = (color) => ({
+    type: 'ASSIGN_COLOR',
+    color
+});
+
+export const frameReseted = (frame) => ({
+    type: 'RESET_FRAME',
+    frame
+});
+
+export const framesDownloaded = (bitmap) => ({
+    type: 'DOWNLOAD_FRAMES',
+    frame
+});
