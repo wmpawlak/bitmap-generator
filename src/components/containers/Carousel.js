a7fd18a0063b31599905e3ff7462b95a9ef6b3ab
Wojtek Pawlak <wojciech.pawlak92@gmail.com>
2019-07-26 15:55:17 +0200

    work in progress on adding frames

---
+++ src/components/containers/Carousel.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Slider from "react-slick";
+import { connect } from "react-redux";
+
+import Frame from "../presentational/Frame";
+
+class CenterMode extends React.Component {
+    renderFrame(i) {
+        return <Frame value={i} />;
+    }
+
+    render() {
+        const settings = {
+            focusOnSelect: true,
+            infinite: true,
+            centerPadding: "0px",
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            speed: 500,
+            variableWidth: true,
+            initalSlide: 1
+        };
+
+        const mapStateToProps = state => ({
+            length: state.setOfSquares.length
+        });
+
+        connect(
+            mapStateToProps,
+            null
+        )(CenterMode);
+
+        return (
+            <div className="carousel">
+                <Slider {...settings}>
+                    {Array.from({ length: this.state.length }).map((s, i) => (
+                        <div key={i}>{this.renderFrame(i)}</div>
+                    ))}
+                </Slider>
+            </div>
+        );
+    }
+}
+
+export default CenterMode;
