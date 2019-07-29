e76155fdbb1e399f1cd78d08e846f1fdf5ab39ea
Wojtek Pawlak <wojciech.pawlak92@gmail.com>
2019-07-29 16:04:44 +0200

    working selection of a frame in a slider

--- src/components/presentational/PreviewFrame.js
+++ src/components/presentational/PreviewFrame.js
@@ -3,13 +3,24 @@ import { connect } from "react-redux";
 
 import { chooseFrame } from "../../actions";
 
-const PreviewFrame = ({ chooseFrame }) => {
+const PreviewFrame = ({ index, chooseFrame }) => {
+    const handleClick = () => {
+        chooseFrame(index)
+    }
     return (
-        <div className="preview-frame" onClick={chooseFrame}></div>
+        <div className="preview-frame" onClick={handleClick}></div>
     );
 };
 
+const mapStateToProps = state => ({
+    activeFrameIndex: state.reducer.activeFrameIndex
+});
+
+const mapDispatchToProps = {
+    chooseFrame: chooseFrame
+}
+
 export default connect(
-    null,
-    { chooseFrame }
+    mapStateToProps,
+    mapDispatchToProps
 )(PreviewFrame);
