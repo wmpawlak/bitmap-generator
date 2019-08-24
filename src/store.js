606c7b055ac6e2e5d7ff78860de9b480a11942f7
Wojtek Pawlak <wojciech.pawlak92@gmail.com>
2019-08-24 21:23:10 +0200

    small tweak - typo

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore, compose } from 'redux';
-import { reducer } from './reducers/reducer';
+import { reducer } from './redux/reducers/reducer';
 
 const enhancers = compose(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
