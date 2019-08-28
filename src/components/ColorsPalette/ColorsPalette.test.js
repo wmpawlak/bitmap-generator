c430231295491918de1005ee6f4deb8a84200af6
Wojtek Pawlak <wojciech.pawlak92@gmail.com>
2019-08-28 18:55:42 +0200

    test of tests on circle

--- src/components/ColorsPalette/ColorsPalette.test.js
+++ src/components/ColorsPalette/ColorsPalette.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ColorsPalette from './ColorsPalette';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import toJson from 'enzyme-to-json';
+//import toJson from 'enzyme-to-json';
 import store from '../../store';
 
 configure({ adapter: new Adapter() });
@@ -10,6 +10,6 @@ configure({ adapter: new Adapter() });
 describe('<ColorsPalette /> props', () => {
   it('contains props with black rgb color', () => {
     const wrapper = shallow(<ColorsPalette store={store} />).dive();
-    expect(wrapper.instance().props.color.toBe('{r: 0, g: 0, b: 0'));
+    expect(wrapper.instance().props.color).toBe('{r: 0, g: 0, b: 0}');
   });
 });
