e2e223b2d465fb21bb8e5b4a306ed4034c077f40
MatGralak <mateuszgralak89@gmail.com>
2019-08-29 16:53:41 +0200

    fixed buttons and carousel

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -69,7 +69,7 @@ const Container = ({
             </div>
           </div>
         </div>
-        <div>
+        <div className="carouselWrapper">
           <Carousel />
         </div>
         <Canvas />
