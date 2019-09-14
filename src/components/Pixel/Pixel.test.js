510c840614733e07228b8fdc61dbf1cee0452e57
Wojtek Pawlak <wojciech.pawlak92@gmail.com>
2019-09-14 18:32:11 +0200

    test for Pixel is working

--- src/components/Pixel/Pixel.test.js
+++ src/components/Pixel/Pixel.test.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import { Pixel } from './Pixel';
-import { configure, shallow } from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import configureMockStore from 'redux-mock-store';
 import { reducer } from '../../redux/reducers/reducer';
-import { chooseColor } from '../../redux/actions';
-import { assignColor } from '../../redux/actions';
+import { assignColor, chooseFrame } from '../../redux/actions';
 
 configure({ adapter: new Adapter() });
 
@@ -17,30 +14,21 @@ describe('<Pixel/>', () => {
   it('checks if the color assigned is a color in store', () => {
     const initialState = createState({
       color: [200, 200, 0],
-      listOfFrames: [[[0, 0, 0]]],
+      listOfFrames: [
+        [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],
+        [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],
+        [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+      ],
       activeFrameIndex: 0
     });
     const store = createMockStore(initialState);
-    console.log(store.getState());
+    console.table(store.getState().listOfFrames);
 
-    store.dispatch(assignColor(0));
-    console.log(store.getState().listOfFrames);
-    // expect(false).toBeTruthy();
+    store.dispatch(chooseFrame(2));
+    store.dispatch(assignColor(4));
+    console.table(store.getState().listOfFrames);
+    console.table(store.getState().listOfFrames[2][4]);
+    expect(store.getState().listOfFrames[2][4]).toEqual([200, 200, 0]);
+    expect(store.getState().listOfFrames[0][0]).toEqual([0, 0, 0]);
   });
 });
-
-//calls assign color with payload of selected color,index
-//sets selected color in initial state
-//checks if color in specific pixel is a color in initial state
-
-//używać it czy test, jak jest lepiej?
-
-
-
-//1. zrobić 2 klatki
-//2. w modyfikowanej 3 pixele 
-//3. modyfikowana to activeFrameIndex
-//4. modyfikacja 2 lub 3 pixela w 2 klatce
-//5. expect - nieruszona klatka [0] zostaje ta sama, 
-//1 lub 2 px w 2 klatce wygląda tak jak wcześniej
-// pixel zmieniony zgadza się z tym co chciałem
